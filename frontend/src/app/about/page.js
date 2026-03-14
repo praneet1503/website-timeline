3cd430d6ac5ccf3b0bf177efb4900cab72ec754b
praneet1503 <prentz.work@gmail.com>
2026-03-14 20:45:14 +0400

    update:about page changes

--- frontend/src/app/about/page.js
+++ frontend/src/app/about/page.js
@@ -7,12 +7,16 @@ export default function AboutPage() {
             <p className="mt-4 text-lg">
                 This project is a personal initiative to explore and visualize the history of websites using the Internet Archive's Wayback Machine. It allows users to see how a website has evolved over time by displaying snapshots taken at different points in history.
             </p>
+            <p> yeah i get it the site is a bit slow but i cant do anything about it so yeah </p>
             <p className="mt-4 text-lg">
                 The frontend is built with Next.js, while the backend is powered by FastAPI. The application fetches data from the Wayback Machine API and presents it in an interactive timeline format.
             </p>
             <p className="mt-4 text-lg">
-                If you're interested in contributing or have any questions, feel free to check out the <Link href="https://github.com/nasapedia" className="text-blue-500 hover:underline">GitHub repository</Link>.
+                If you're interested in contributing or have any questions, feel free to check out the <Link href="https://github.com/praneet1503/website-timeline" className="text-blue-500 hover:underline">GitHub repository</Link>.
             </p>
+            <p>the website is built with Next.js and styled with Tailwind CSS.</p>
+            <p> the text is jetbrains mono if you the like text.(i liked it obv so much)</p>
+            <p>and if you have any suggestions on improving the wbeiste just hit me up <a href="https://hackclub.enterprise.slack.com/team/U07V12XBSF6" className="text-blue-500 hover:underline">on Slack</a></p>
         </main>
     );
 }
